822577922d2725b04173f90ec3df906c24da287b
Elliott Moore <elliott-moore@outlook.com>
2018-05-02 16:15:55 +1000

    removing code not required

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div className="App ui middle aligned center aligned grid">
+      <div className="column">yeet</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/colour-palette.md
@@ -0,0 +1,9 @@
+colour-palette
+
+bg - #fafafa
+
+copy(muted) - #6d6f7b || #5b5e6d
+
+copy - #1F1021
+
+cta - #FFEE3C || #FDEC00
