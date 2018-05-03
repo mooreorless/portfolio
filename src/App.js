ee365d61a8385c2d8dda56786f19f46d38c137ac
Elliott Moore <elliott-moore@outlook.com>
2018-05-03 17:47:36 +1000

    basic structure complete - need to add dynamic project list and cta on aws

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 
-import './App.css';
+import { Welcome, About, Project, Copyright } from './components';
+import './App.scss';
 
 
 const App = () => {
   return (
-    <div className="App ui middle aligned center aligned grid">
-      <div className="column">yeet</div>
+    <div className="App">
+      <Container text className="middle aligned grid">
+        <div className="column">
+          <Welcome />
+          <About />
+          <Project />
+        </div>
+      </Container>
+      <Copyright />
     </div>
   );
 }
--- src/colour-palette.md
+++ src/colour-palette.md
@@ -6,4 +6,4 @@ copy(muted) - #6d6f7b || #5b5e6d
 
 copy - #1F1021
 
-cta - #FFEE3C || #FDEC00
+cta - #FFEE3C || #FDEC00 || #FFD700
